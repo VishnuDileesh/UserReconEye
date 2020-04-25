fd8500d350f528b802bb86f04eb0417fd2850552
Pheyonix <dev@vishnudileesh.com>
2020-04-25 11:59:50 +0530

    Add banner and username argument passing

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ from tqdm import tqdm
 from pyfiglet import Figlet
 from helium import *
 import click
+from platforms_list import platforms
+
+f = Figlet(
+        font='slant',
+        width=80
+        )
+
+__author__ = 'vishnu_dileesh'
+
+click.echo(click.style(f.renderText('UserReconEye'), bold=True, fg='cyan'))
+click.echo(click.style((f'Author: vishnu_dileesh'), fg='cyan'))
+
+@click.command()
+@click.argument('username')
+def main(username):
+    click.echo('name is {}'.format(username))
 
 
 
-
-
+if __name__ == "__main__":
+    main()

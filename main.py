0f96354d0db777fc944b8172e396a031a4295e27
Pheyonix <dev@vishnudileesh.com>
2020-04-25 20:24:09 +0530

    Generate HTML report and serve

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from sys import argv
 from tqdm import tqdm
 from pyfiglet import Figlet
 from helium import *
+import dominate
 import click
 import tldextract
+import http.server
+import socketserver
+from dominate.tags import *
 from platforms_list import platforms
+
 
 
 f = Figlet(
@@ -47,6 +52,10 @@ def main(username):
             reconsearch(site, username)
 
 
+    render_report(username)
+
+enum_report = {}
+
 
 def reconsearch(site, username):
 
@@ -60,8 +69,48 @@ def reconsearch(site, username):
 
     get_driver().save_screenshot(src_name)
 
+    enum_report[site_name.domain] = [site, src_name]
 
     kill_browser()
+
+
+
+def render_report(username):
+    
+    doc = dominate.document(title='Enumeration Report : {}'.format(username))
+
+    click.echo('Generating report of {}'.format(username))
+
+    for k, v in tqdm(enum_report.items()):
+
+        site_link = v[0]
+
+        with doc.body:
+            with a(href=site_link):
+                img(src='screenshots/{}/{}.png'.format(username, k))
+
+    with open('{}_report.html'.format(username), 'w') as f:
+        f.write(doc.render())
+
+    PORT = 4700
+    Handler = http.server.SimpleHTTPRequestHandler
+
+ 
+
+    try: 
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            click.echo('Report Generated Successfuly')
+            click.echo('Open report at http://localhost:{}/{}_report.html'.format(PORT, username))
+
+            httpd.serve_forever()
+
+    except:
+        print('Port is busy, switching port')
+        PORT += 3
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            click.echo('Report Generated Successfuly')
+            click.echo('Open report at http://localhost:{}/{}.html'.format(PORT, username))
+            httpd.serve_forever()
 
 
 if __name__ == "__main__":
